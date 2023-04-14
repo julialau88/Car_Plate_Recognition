cb47a28fb2406af0206bf80b02067c70aed75cf8
julia <julia@thunderquote.com>
2023-04-14 15:14:10 +0800

    Added unsharp

---
+++ unsharp.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image, ImageFilter, ImageChops
+
+def unsharp(kernel_size, img):
+    kernel = kernel_size**2
+
+    k = np.ones(kernel)/kernel
+
+    # img_blur = img.filter(ImageFilter.Kernel((kernel_size, kernel_size), k))
+    img_blur = img.filter(ImageFilter.MedianFilter((3)))
+
+    unsharp_mask = ImageChops.subtract(img, img_blur)
+
+    sharped_img = ImageChops.add(img, unsharp_mask)
+
+    return sharped_img
