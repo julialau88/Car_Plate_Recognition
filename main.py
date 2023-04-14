bb4bfdf4fda2a82238aa76f467bf67f7f3d963d7
julia <julia@thunderquote.com>
2023-04-14 15:14:50 +0800

    Main.py updated with resizing, smoothing, & sobel

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 from sobel import vertical_sobel 
 from PIL import Image
 import numpy as np
+from gaussian import gaussian
+from unsharp import unsharp
 
 def main():
-    img = Image.open("Images/test_img1.png").convert('L')
-    size = (384, 288)
+    img = Image.open("Images/test_img4.png").convert('L')
+    size = (1440, 1440)     # Resize to 1080 by 1080 
     img =  img.resize(size)
     img_arr = np.array(img)
-    print(img_arr)
+    img = gaussian(5, img)
+
+    img = unsharp(3, img)
+    # print(img_arr)
+
     # width, height = img.size
 
     vertical_sobel(img, img_arr)
